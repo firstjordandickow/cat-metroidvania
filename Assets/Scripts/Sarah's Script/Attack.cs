0925588c69e88d0dbec7de515ba1c59f9e664c0f
Niipanda3 <niipanda3@gmail.com>
2023-02-10 17:24:02 -0600

    Sarah added Attack Script to Player Character

--- Assets/Scripts/Sarah's Script/Attack.cs
+++ Assets/Scripts/Sarah's Script/Attack.cs
@@ -7,12 +7,13 @@ public class Attack : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+    
         
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    { 
+        Debug.Log("Hello world");
     }
 }
