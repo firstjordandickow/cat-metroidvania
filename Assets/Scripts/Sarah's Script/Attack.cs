b9fe7576af403a10531989078b07f5527766f30c
Niipanda3 <niipanda3@gmail.com>
2023-02-10 17:58:49 -0600

    Sarah added basic attack function

--- Assets/Scripts/Sarah's Script/Attack.cs
+++ Assets/Scripts/Sarah's Script/Attack.cs
@@ -19,17 +19,14 @@ public class Attack : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        Debug.Log("colliding");
+    if(collision.gameObject.name =="Test Enemy"){
+        Destroy(collision.gameObject);}
     }
 
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        
-    }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        
-    }
+   
+  
+    
 
 
 
